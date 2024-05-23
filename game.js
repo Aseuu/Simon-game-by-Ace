f3cfd41d581f11d4ecaa7881e944aaf8fa5691f3
Aseu <76776082+Aseuu@users.noreply.github.com>
2024-05-23 03:34:23 +0100

    Update game.js

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ $(document).keypress(function() {
   }
 });
 
-$("#start").click(function() {
+$(".start").click(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
